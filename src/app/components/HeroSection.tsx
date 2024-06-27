7199605d20f464b3569f7996a569e5ed0064d193
margretedeh <sagaedeh@gmail.com>
2024-06-27 10:45:16 -0400

    tab screen manage

--- src/app/components/HeroSection.tsx
+++ src/app/components/HeroSection.tsx
@@ -7,9 +7,9 @@ interface IHeroSectionProps {}
 
 const HeroSection: React.FunctionComponent<IHeroSectionProps> = (props) => {
   return (
-    <main className="flex md:h-[500px] md:px-20 md:py-20   ">
-      <article className=" md:w-[40%]  bg-white flex flex-col px-3 py-5 items-center justify-center md:items-start md:justify-start gap-3 ">
-        <h2 className=" text-xl text-center md:text-start md:text-3xl font-semibold md:leading-10">
+    <main className="flex lg:h-[500px] md:px-20 md:py-20   ">
+      <article className=" lg:w-[40%]  bg-white flex flex-col px-3 py-5 items-center justify-center lg:items-start lg:justify-start gap-3 ">
+        <h2 className=" text-xl text-center lg:text-start lg:text-3xl font-semibold lg:leading-10">
           SINC Partners is a service incubation company
         </h2>
         <p className="md:text-lg leading-8">
@@ -20,7 +20,7 @@ const HeroSection: React.FunctionComponent<IHeroSectionProps> = (props) => {
         </p>
         <Button className=" rounded-3xl bg-dark text-sm  text-white px-5 py-3">SINC With Us</Button>
       </article>
-      <Image className=" filter opacity-20 md:opacity-100 end-0 absolute top-0" src={hero} alt="Hero"/>
+      <Image className=" filter opacity-20  lg:opacity-100 end-0 absolute top-0" src={hero} alt="Hero"/>
     </main>
   );
 };
